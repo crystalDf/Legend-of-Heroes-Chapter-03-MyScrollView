92ccb5602301e1d89649deafb2082bdba135c984
crystalDf <chendong333@gmail.com>
2017-02-28 21:58:05 +0800

    Chapter 03 MyScrollView

---
+++ app/src/main/java/com/star/myscrollview/MyScrollView.java
@@ -0,0 +1,18 @@
+package com.star.myscrollview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+
+public class MyScrollView extends ViewGroup {
+
+    public MyScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
