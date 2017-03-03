07f197624d9a5127cf60b2a523cb559e4368778f
crystalDf <chendong333@gmail.com>
2017-03-03 22:05:46 +0800

    Chapter 03 MyScrollView

--- app/src/main/java/com/star/myscrollview/MyScrollView.java
+++ app/src/main/java/com/star/myscrollview/MyScrollView.java
@@ -3,6 +3,7 @@ package com.star.myscrollview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 
@@ -15,6 +16,14 @@ public class MyScrollView extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            View childView = getChildAt(i);
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+
     }
 
     @Override
@@ -26,4 +35,5 @@ public class MyScrollView extends ViewGroup {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
     }
+
 }
